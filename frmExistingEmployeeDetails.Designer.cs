107e208ef43b8fb90ebb8d5b60223c5a6c539601
EmileAFourie <109984598+EmileAFourie@users.noreply.github.com>
2023-08-14 19:53:50 +0200

    changes

--- frmExistingEmployeeDetails.Designer.cs
+++ frmExistingEmployeeDetails.Designer.cs
@@ -28,217 +28,216 @@
         /// </summary>
         private void InitializeComponent()
         {
-            btnGenerateReport = new Button();
-            btnDelete = new Button();
-            label6 = new Label();
-            txtbxAge = new TextBox();
-            label5 = new Label();
-            txtbxPosition = new TextBox();
-            label4 = new Label();
-            txtbxEmail = new TextBox();
-            lblName = new Label();
-            txtbxName = new TextBox();
-            lblExistingEmployee = new Label();
-            lblEmployeeID = new Label();
-            txtbxEmployeeID = new TextBox();
-            lblPerformance = new Label();
-            textBox1 = new TextBox();
-            btnUpdate = new Button();
-            lblAssignedTickets = new Label();
-            dataGridView1 = new DataGridView();
-            button1 = new Button();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
-            SuspendLayout();
+            this.btnGenerateReport = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtbxAge = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtbxPosition = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtbxEmail = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtbxName = new System.Windows.Forms.TextBox();
+            this.lblExistingEmployee = new System.Windows.Forms.Label();
+            this.lblEmployeeID = new System.Windows.Forms.Label();
+            this.txtbxEmployeeID = new System.Windows.Forms.TextBox();
+            this.lblPerformance = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.lblAssignedTickets = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
             // 
             // btnGenerateReport
             // 
-            btnGenerateReport.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            btnGenerateReport.Location = new Point(659, 553);
-            btnGenerateReport.Margin = new Padding(3, 4, 3, 4);
-            btnGenerateReport.Name = "btnGenerateReport";
-            btnGenerateReport.Size = new Size(127, 31);
-            btnGenerateReport.TabIndex = 25;
-            btnGenerateReport.Text = "Generate Report";
-            btnGenerateReport.UseVisualStyleBackColor = true;
+            this.btnGenerateReport.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnGenerateReport.Location = new System.Drawing.Point(659, 553);
+            this.btnGenerateReport.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnGenerateReport.Name = "btnGenerateReport";
+            this.btnGenerateReport.Size = new System.Drawing.Size(127, 31);
+            this.btnGenerateReport.TabIndex = 25;
+            this.btnGenerateReport.Text = "Generate Report";
+            this.btnGenerateReport.UseVisualStyleBackColor = true;
             // 
             // btnDelete
             // 
-            btnDelete.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            btnDelete.Location = new Point(120, 556);
-            btnDelete.Margin = new Padding(3, 4, 3, 4);
-            btnDelete.Name = "btnDelete";
-            btnDelete.Size = new Size(86, 31);
-            btnDelete.TabIndex = 24;
-            btnDelete.Text = "Delete";
-            btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnDelete.Location = new System.Drawing.Point(120, 556);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(86, 31);
+            this.btnDelete.TabIndex = 24;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
             // 
             // label6
             // 
-            label6.AutoSize = true;
-            label6.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label6.Location = new Point(87, 364);
-            label6.Name = "label6";
-            label6.Size = new Size(41, 20);
-            label6.TabIndex = 23;
-            label6.Text = "Age:";
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(87, 364);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 15);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Age:";
             // 
             // txtbxAge
             // 
-            txtbxAge.Location = new Point(147, 353);
-            txtbxAge.Margin = new Padding(3, 4, 3, 4);
-            txtbxAge.Name = "txtbxAge";
-            txtbxAge.Size = new Size(404, 27);
-            txtbxAge.TabIndex = 22;
+            this.txtbxAge.Location = new System.Drawing.Point(147, 353);
+            this.txtbxAge.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtbxAge.Name = "txtbxAge";
+            this.txtbxAge.Size = new System.Drawing.Size(404, 23);
+            this.txtbxAge.TabIndex = 22;
             // 
             // label5
             // 
-            label5.AutoSize = true;
-            label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label5.Location = new Point(62, 304);
-            label5.Name = "label5";
-            label5.Size = new Size(70, 20);
-            label5.TabIndex = 21;
-            label5.Text = "Position:";
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(62, 304);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 15);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Position:";
             // 
             // txtbxPosition
             // 
-            txtbxPosition.Location = new Point(147, 300);
-            txtbxPosition.Margin = new Padding(3, 4, 3, 4);
-            txtbxPosition.Name = "txtbxPosition";
-            txtbxPosition.Size = new Size(404, 27);
-            txtbxPosition.TabIndex = 20;
+            this.txtbxPosition.Location = new System.Drawing.Point(147, 300);
+            this.txtbxPosition.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtbxPosition.Name = "txtbxPosition";
+            this.txtbxPosition.Size = new System.Drawing.Size(404, 23);
+            this.txtbxPosition.TabIndex = 20;
             // 
             // label4
             // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label4.Location = new Point(77, 251);
-            label4.Name = "label4";
-            label4.Size = new Size(51, 20);
-            label4.TabIndex = 19;
-            label4.Text = "Email:";
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(77, 251);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 15);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Email:";
             // 
             // txtbxEmail
             // 
-            txtbxEmail.Location = new Point(147, 240);
-            txtbxEmail.Margin = new Padding(3, 4, 3, 4);
-            txtbxEmail.Name = "txtbxEmail";
-            txtbxEmail.Size = new Size(404, 27);
-            txtbxEmail.TabIndex = 18;
+            this.txtbxEmail.Location = new System.Drawing.Point(147, 240);
+            this.txtbxEmail.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtbxEmail.Name = "txtbxEmail";
+            this.txtbxEmail.Size = new System.Drawing.Size(404, 23);
+            this.txtbxEmail.TabIndex = 18;
             // 
             // lblName
             // 
-            lblName.AutoSize = true;
-            lblName.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            lblName.Location = new Point(74, 189);
-            lblName.Name = "lblName";
-            lblName.Size = new Size(55, 20);
-            lblName.TabIndex = 17;
-            lblName.Text = "Name:";
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblName.Location = new System.Drawing.Point(74, 189);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(43, 15);
+            this.lblName.TabIndex = 17;
+            this.lblName.Text = "Name:";
             // 
             // txtbxName
             // 
-            txtbxName.Location = new Point(147, 185);
-            txtbxName.Margin = new Padding(3, 4, 3, 4);
-            txtbxName.Name = "txtbxName";
-            txtbxName.Size = new Size(404, 27);
-            txtbxName.TabIndex = 16;
+            this.txtbxName.Location = new System.Drawing.Point(147, 185);
+            this.txtbxName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtbxName.Name = "txtbxName";
+            this.txtbxName.Size = new System.Drawing.Size(404, 23);
+            this.txtbxName.TabIndex = 16;
             // 
             // lblExistingEmployee
             // 
-            lblExistingEmployee.AutoSize = true;
-            lblExistingEmployee.Font = new Font("Segoe UI", 20F, FontStyle.Bold, GraphicsUnit.Point);
-            lblExistingEmployee.Location = new Point(270, 45);
-            lblExistingEmployee.Name = "lblExistingEmployee";
-            lblExistingEmployee.Size = new Size(429, 46);
-            lblExistingEmployee.TabIndex = 15;
-            lblExistingEmployee.Text = "Existing Employee Details";
+            this.lblExistingEmployee.AutoSize = true;
+            this.lblExistingEmployee.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblExistingEmployee.Location = new System.Drawing.Point(270, 45);
+            this.lblExistingEmployee.Name = "lblExistingEmployee";
+            this.lblExistingEmployee.Size = new System.Drawing.Size(347, 37);
+            this.lblExistingEmployee.TabIndex = 15;
+            this.lblExistingEmployee.Text = "Existing Employee Details";
             // 
             // lblEmployeeID
             // 
-            lblEmployeeID.AutoSize = true;
-            lblEmployeeID.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            lblEmployeeID.Location = new Point(38, 140);
-            lblEmployeeID.Name = "lblEmployeeID";
-            lblEmployeeID.Size = new Size(97, 20);
-            lblEmployeeID.TabIndex = 14;
-            lblEmployeeID.Text = "EmployeeID:";
+            this.lblEmployeeID.AutoSize = true;
+            this.lblEmployeeID.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblEmployeeID.Location = new System.Drawing.Point(38, 140);
+            this.lblEmployeeID.Name = "lblEmployeeID";
+            this.lblEmployeeID.Size = new System.Drawing.Size(77, 15);
+            this.lblEmployeeID.TabIndex = 14;
+            this.lblEmployeeID.Text = "EmployeeID:";
             // 
             // txtbxEmployeeID
             // 
-            txtbxEmployeeID.Location = new Point(147, 136);
-            txtbxEmployeeID.Margin = new Padding(3, 4, 3, 4);
-            txtbxEmployeeID.Name = "txtbxEmployeeID";
-            txtbxEmployeeID.Size = new Size(404, 27);
-            txtbxEmployeeID.TabIndex = 13;
+            this.txtbxEmployeeID.Location = new System.Drawing.Point(147, 136);
+            this.txtbxEmployeeID.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtbxEmployeeID.Name = "txtbxEmployeeID";
+            this.txtbxEmployeeID.Size = new System.Drawing.Size(404, 23);
+            this.txtbxEmployeeID.TabIndex = 13;
             // 
             // lblPerformance
             // 
-            lblPerformance.AutoSize = true;
-            lblPerformance.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            lblPerformance.Location = new Point(32, 409);
-            lblPerformance.Name = "lblPerformance";
-            lblPerformance.Size = new Size(103, 20);
-            lblPerformance.TabIndex = 27;
-            lblPerformance.Text = "Performance:";
+            this.lblPerformance.AutoSize = true;
+            this.lblPerformance.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblPerformance.Location = new System.Drawing.Point(32, 409);
+            this.lblPerformance.Name = "lblPerformance";
+            this.lblPerformance.Size = new System.Drawing.Size(83, 15);
+            this.lblPerformance.TabIndex = 27;
+            this.lblPerformance.Text = "Performance:";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(147, 409);
-            textBox1.Margin = new Padding(3, 4, 3, 4);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(404, 27);
-            textBox1.TabIndex = 26;
+            this.textBox1.Location = new System.Drawing.Point(147, 409);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(404, 23);
+            this.textBox1.TabIndex = 26;
             // 
             // btnUpdate
             // 
-            btnUpdate.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            btnUpdate.Location = new Point(299, 464);
-            btnUpdate.Margin = new Padding(3, 4, 3, 4);
-            btnUpdate.Name = "btnUpdate";
-            btnUpdate.Size = new Size(86, 31);
-            btnUpdate.TabIndex = 28;
-            btnUpdate.Text = "Update";
-            btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnUpdate.Location = new System.Drawing.Point(299, 464);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(86, 31);
+            this.btnUpdate.TabIndex = 28;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
             // 
             // lblAssignedTickets
             // 
-            lblAssignedTickets.AutoSize = true;
-            lblAssignedTickets.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            lblAssignedTickets.Location = new Point(673, 116);
-            lblAssignedTickets.Name = "lblAssignedTickets";
-            lblAssignedTickets.Size = new Size(126, 20);
-            lblAssignedTickets.TabIndex = 29;
-            lblAssignedTickets.Text = "Assigned Tickets";
+            this.lblAssignedTickets.AutoSize = true;
+            this.lblAssignedTickets.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblAssignedTickets.Location = new System.Drawing.Point(673, 116);
+            this.lblAssignedTickets.Name = "lblAssignedTickets";
+            this.lblAssignedTickets.Size = new System.Drawing.Size(99, 15);
+            this.lblAssignedTickets.TabIndex = 29;
+            this.lblAssignedTickets.Text = "Assigned Tickets";
             // 
             // dataGridView1
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(593, 140);
-            dataGridView1.Margin = new Padding(3, 4, 3, 4);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.Size = new Size(274, 391);
-            dataGridView1.TabIndex = 30;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(593, 140);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(274, 391);
+            this.dataGridView1.TabIndex = 30;
             // 
             // button1
             // 
-            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            button1.Location = new Point(12, 556);
-            button1.Margin = new Padding(3, 4, 3, 4);
-            button1.Name = "button1";
-            button1.Size = new Size(86, 31);
-            button1.TabIndex = 31;
-            button1.Text = "Back";
-            button1.UseVisualStyleBackColor = true;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(12, 556);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 31);
+            this.button1.TabIndex = 31;
+            this.button1.Text = "Back";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // frmExistingEmployeeDetails
             // 
-
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(947, 635);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.lblAssignedTickets);
             this.Controls.Add(this.btnUpdate);
@@ -262,7 +261,6 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
-
 
         }
 
