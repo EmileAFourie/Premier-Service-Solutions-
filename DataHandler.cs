1af91cacfa60f6223625f8d7bd57f65553a741d2
Sir-Dovah <ajroux7@gmail.com>
2023-08-15 14:09:32 +0200

    Updated Datahandler

--- DataHandler.cs
+++ DataHandler.cs
@@ -16,16 +16,105 @@ namespace Premier_Service_Solutions
         SqlCommand cmd;
         SqlDataReader reader;
 
+        //========================================================================================================================
+        //Clients
+        //=====================================================================================================================
+
         public void ClientIWriteToDB(int ClientID, string Firstname, string Surname, string Address, string CellNo, string Email, int ContractID, int CompanyID)
         {
-            string insert = "Insert INTO [dbo].[IndividualClient] Values  ";
+            string insertI = @"Insert INTO [dbo].[IndividualClient] Values ('"+ClientID+ "','" + Firstname + "','" + Surname + "'," +
+                "'" + Address + "','" + CellNo + "','" + Email + "','" + ContractID + "','" + CompanyID + "') ";
 
+            conn = new SqlConnection(connect);
+            conn.Open();
+
+            cmd = new SqlCommand(insertI, conn);
+
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Individual Client details saved successfully");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Details not saved" + ex);
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
 
         public void ClientBWriteToDB(int CompanyID, string CompanyName, string CompanyAddress, string CompanyPhone)
         {
-            string insert = "Insert INTO [dbo].[CompanyClient] Values   ";
+            string insertB = @"Insert INTO [dbo].[IndividualClient] Values ('" + CompanyID + "','" + CompanyName + "','" + CompanyAddress + "'," +
+             "'" + CompanyPhone + "') ";
+
+            conn = new SqlConnection(connect);
+            conn.Open();
+
+            cmd = new SqlCommand(insertB, conn);
+
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Individual Client details saved successfully");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Details not saved" + ex);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        //=========================================================================================================================================
+        // Tickects
+        //======================================================================================================================================================
+        
+        public void LogTickect(int TicketID, string TypeOfErr, string Description, DateTime Dateopened, DateTime DateClosed, string Priority, string Status, int ClientID)
+        {
+            string insert = @"Insert INTO [dbo].[Ticket] Values ('"+ TicketID + "','" + TypeOfErr + "','" + Description + "'," +
+                "'" + Dateopened + "','" + DateClosed + "','" + Priority + "','" + Status + "','" + ClientID + "',) ";
+
+            conn = new SqlConnection(connect);
+            conn.Open();
+            
+            cmd = new SqlCommand(insert, conn);
+            
+            
+            // Put status as unassigned by default
+
+
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Ticket has been logged");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ticket not logged" + ex);
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
 
+
+        //============================================================================
+        //Employee
+        //============================================================================
+
+        
+        //============================================================================
+        //Contracts
+        //============================================================================
+
+
     }
+
 }
